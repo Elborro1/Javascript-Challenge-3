67b14c5ab937fd53518f98146c993bd8f25becd7
Dean Warwick <elborro@gmail.com>
2022-06-07 14:32:22 +0200

    Coding Challenge 3

---
+++ script.js
@@ -0,0 +1,104 @@
+
+//type conversion
+
+// const inputYear = '1991';
+// console.log(inputYear + 18);
+// console.log(Number(inputYear) + 18);
+ 
+// console.log(Number("Dean"));
+// console.log(typeof NaN)
+
+// console.log(String(23), 23);
+
+// // type coercion
+
+// console.log('I am ' + 23 + 'years old');
+// console.log('23' - '10' -3);
+// console.log('23' * '2')
+
+// let n = '1' + 1; // '11'
+// n = n - 1;          // '11' - 1
+
+// console.log(n)
+
+//Truthy and Falsey Values://
+
+// 5 Falsy values: 0, '', undefined, null, NaN.//
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Dean'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// const money = 0;
+
+// if(money) {
+//     console.log("Don't spend it all")
+// }else{
+//     console.log("You should get a job")
+// }
+
+// let height ; // height is undefined (no value)
+// if(height){
+//     console.log( " yay height is defined");
+// }else{
+//     console.log("height is UNDEFINED");
+// }
+
+//Equality operators://
+
+// const age = '18';
+
+// if(age === 18) ;
+// console.log('You are an adult (strict)');
+   
+// // === is sctrict//
+// // == is type coercion//
+
+// if(age == 18); 
+// console.log('You are an adult (loose)');
+// // When comparing values, always use strict //
+
+// const favorite = Number(prompt("what's your favorite number"));
+
+// console.log(favorite);
+// console.log(typeof favorite);
+
+// if(favorite === 23){
+//     console.log("cool");
+// }else if(favorite ===7){
+//         console.log("also cool number")
+    
+// }else
+//     console.log("Not cool man");
+
+// //different operator//
+//     if(favorite !== 23) console.log("why not 23");
+
+// Boolean logic//
+
+// const hasDriversLicense = true; //A
+// const hasGoodVision = true; //B
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+
+
+// // if( hasDriversLicense && hasGoodVision){
+// //     console.log("Sarah should drive!");
+// // } else {
+// //     console.log("Sarah should not drive!");
+// // }
+
+// const isTired = true 
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// if(hasDriversLicense && hasGoodVision && !isTired){
+//     console.log("Sarah can drive")
+// } else {
+//     console.log("Someone else drive")
+// }
+
